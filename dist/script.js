74966b52445c8a6a4ebe950190d1b468d79e71bc
Yuvraj Malhotra <yocktherock1990@gmail.com>
2024-01-08 14:46:25 +0530

    Updated uncompressed script

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,264 @@
-"use strict";const body=document.body,container=document.getElementById("container"),input=document.getElementById("search"),menu=document.getElementById("suggestions_menu"),overlay=document.getElementById("overlay"),linkArrow=document.getElementById("link_arrow"),userCard=document.getElementById("user_card"),userImage=document.getElementById("user_image"),userDescreption=document.getElementById("user_descreption"),userName=document.getElementById("user_name"),userBio=document.getElementById("user_bio"),userFollower=document.getElementById("user_followers"),userFollowing=document.getElementById("user_following"),userUsername=document.getElementById("user_username"),userURL=document.getElementById("user_url");let intervalId,userData=[],index=-1;const headers={Authorization:"token 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","Content-Type":"application/json"},loadBodyBackground=()=>{let e=new Image;e.src="img/space.jpg",e.addEventListener("load",()=>{body.style.backgroundImage=`url('${e.src}')`})},unselect=()=>{index=-1,userData&&userData.forEach(e=>{e.classList.remove("bg-[#484f6683]")})},showUserCard=()=>{overlay.classList.add("custom"),container.classList.add("custom"),userCard.classList.add("custom")},hideUserCard=()=>{overlay.classList.remove("custom"),container.classList.remove("custom"),userCard.classList.remove("custom")},deleteMenu=()=>{index=-1,menu.innerHTML=""},loadAvatar=(e,t)=>{let n=new Image;n.src=e,n.addEventListener("load",()=>{t.src=n.src})},highlight=e=>{let t=userData[e];userData.forEach(e=>{e.classList.remove("bg-[#484f6683]")}),t.classList.add("bg-[#484f6683]")},getAverageColor=async e=>{var t,n,r,a,s=document.createElement("canvas"),i=s.getContext&&s.getContext("2d"),d={r:0,g:0,b:0},l=0;r=s.height=e.naturalHeight||e.offsetHeight||e.height,n=s.width=e.naturalWidth||e.offsetWidth||e.width,i.drawImage(e,0,0),a=(t=i.getImageData(0,0,n,r)).data.length;for(var o=0;o<a;o+=4)d.r+=t.data[o],d.g+=t.data[o+1],d.b+=t.data[o+2],l++;d.r=Math.floor(d.r/l),d.g=Math.floor(d.g/l),d.b=Math.floor(d.b/l)},renderCard=async function(){let e=userData[index].getAttribute("name");console.log(e);let t=await fetch(`https://api.github.com/users/${e}`,{headers}).then(e=>e.json()).then(e=>e);input.value="",userURL.href=t.html_url,userImage.src="img/github-user.png",userName.textContent=t.name,userUsername.textContent=t.login,userFollower.textContent=t.followers,userFollowing.textContent=t.following,loadAvatar(t.avatar_url,userImage);let n;n=t.bio?t.bio:"No bio :-(";let r=new Typewriter(userBio,{delay:30});r.typeString(n).start(),deleteMenu(),showUserCard(),deleteMenu()},hideCard=function(){hideUserCard()},renderMenu=function(){menu.style.display="",menu.innerHTML="",userData=userData.map((e,t)=>{let n=`<li class="user_list p-3 flex rounded-lg m-2 transition-all duration-100" data-index='${t}' name='${e.login}'>&nbsp;<img src="img/github-user.png" alt="${e.login} png" class="h-10 my-auto rounded-full" /><h5 class="leading-10 transition duration-300 ">&nbsp;${e.login}&nbsp;</h5></li>`;return menu.insertAdjacentHTML("beforeend",n),loadAvatar(e.avatar_url,menu.lastChild.querySelector("img")),menu.lastChild})},displaySuggestions=async function(e){try{userData=await fetch(`https://api.github.com/search/users?q=${e}&per_page=5&page=1`).then(e=>{if(!e.ok)throw Error("Data is not fetched");return e.json()}).then(e=>e.items).catch(e=>{}),renderMenu()}catch(t){}};body.addEventListener("click",e=>{e.target==overlay&&(menu.style.display="none",hideCard())}),document.addEventListener("keydown",e=>{"Escape"==e.key&&hideCard()}),menu.addEventListener("mouseover",e=>{let t=e.target.closest(".user_list");t&&highlight(index=t&&e.target.closest(".user_list").dataset.index)}),menu.addEventListener("click",renderCard),input.addEventListener("keydown",e=>{let t=e.key;"ArrowDown"==t||"ArrowUp"==t?("ArrowDown"==t?index++:index<=0?index=userData.length-1:index--,highlight(index=index&&index%userData.length)):"Enter"==t?(-1==index&&(index=0),userData.length&&renderCard()):(index=-1,clearTimeout(intervalId),input.value.length>1?intervalId=setTimeout(function(){input.value.length&&displaySuggestions(input.value)},200):deleteMenu())}),input.addEventListener("click",()=>{menu.style.display="",unselect()}),userUsername.addEventListener("mouseover",e=>{linkArrow.classList.add("custom")}),userUsername.addEventListener("mouseout",()=>{linkArrow.classList.remove("custom")}),loadBodyBackground();
+'use strict';
+
+/***** Declare Variables *******************************************/
+const body = document.body;
+const container = document.getElementById('container');
+const input = document.getElementById('search');
+const menu = document.getElementById('suggestions_menu');
+const overlay = document.getElementById('overlay');
+const linkArrow = document.getElementById('link_arrow');
+const userCard = document.getElementById('user_card');
+const userImage = document.getElementById('user_image');
+const userDescreption = document.getElementById('user_descreption');
+const userName = document.getElementById('user_name');
+const userBio = document.getElementById('user_bio');
+const userFollower = document.getElementById('user_followers');
+const userFollowing = document.getElementById('user_following');
+const userUsername = document.getElementById('user_username');
+const userURL = document.getElementById('user_url');
+let intervalId;
+let userData = [];
+let index = -1;
+
+/********** Arrow Functions (not hoisted) ******************************************/
+
+// Functions with concise arrow syntax, not hoisted
+const loadBodyBackground = () => {
+    const tempImage = new Image();
+    tempImage.src = 'img/space.jpg';
+    tempImage.addEventListener('load', () => {
+        body.style.backgroundImage = `url('${tempImage.src}')`;
+    });
+};
+
+const unselect = () => {
+    index = -1;
+    userData     &&
+        userData.forEach((user) => {
+            user.classList.remove('bg-[#484f6683]');
+        });
+};
+
+const showUserCard = () => {
+    overlay.classList.add('custom');
+    container.classList.add('custom');
+    userCard.classList.add('custom');
+};
+
+const hideUserCard = () => {
+    overlay.classList.remove('custom');
+    container.classList.remove('custom');
+    userCard.classList.remove('custom');
+};
+
+const deleteMenu = () => {
+    index = -1;
+    menu.innerHTML = '';
+};
+
+const loadAvatar = (url, original) => {
+    const tempImage = new Image();
+    tempImage.src = url;
+    tempImage.addEventListener('load', () => {
+        original.src = tempImage.src;
+    });
+};
+
+const highlight = (index) => {
+    const ele = userData[index];
+    userData.forEach((user) => {
+        user.classList.remove('bg-[#484f6683]');
+    });
+    ele.classList.add('bg-[#484f6683]');
+};
+
+const getAverageColor = async (tempImage) => {
+    var canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        imgData,
+        width,
+        height,
+        length,
+        rgb = { r: 0, g: 0, b: 0 },
+        count = 0;
+
+    height = canvas.height = tempImage.naturalHeight || tempImage.offsetHeight || tempImage.height;
+    width = canvas.width = tempImage.naturalWidth || tempImage.offsetWidth || tempImage.width;
+
+    context.drawImage(tempImage, 0, 0);
+
+    imgData = context.getImageData(0, 0, width, height);
+
+    length = imgData.data.length;
+
+    for (var i = 0; i < length; i += 4) {
+        rgb.r += imgData.data[i];
+        rgb.g += imgData.data[i + 1];
+        rgb.b += imgData.data[i + 2];
+        count++;
+    }
+
+    rgb.r = Math.floor(rgb.r / count);
+    rgb.g = Math.floor(rgb.g / count);
+    rgb.b = Math.floor(rgb.b / count);
+};
+
+/**************** Functions. ********************/
+// Function to render user card information
+const renderCard = async function () {
+    // Async function to fetch user data and render the card
+
+    const selectedUser = userData[index].getAttribute('name');
+    console.log(selectedUser);
+    const user =await fetch(`https://api.github.com/users/${selectedUser}`)
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch(err=>{});  
+
+    input.value = '';
+    userURL.href = user.html_url;
+    userImage.src="img/github-user.png";
+    userName.textContent = user.name;
+    userUsername.textContent = user.login;
+    userFollower.textContent = user.followers;
+    userFollowing.textContent = user.following;
+
+    loadAvatar(user.avatar_url,userImage);
+    let typeIt;
+    if (!user.bio) typeIt = 'No bio :-(';
+    else typeIt = user.bio;
+
+    const typewriter = new Typewriter(userBio, {
+        delay: 30,
+    });
+
+    typewriter.typeString(typeIt).start();
+
+    deleteMenu();
+    showUserCard();
+    deleteMenu();
+};
+
+const hideCard = function () {
+    hideUserCard();
+};
+// Function to render the suggestion menu
+const renderMenu = function () {
+    menu.style.display = '';
+    menu.innerHTML = '';
+    userData = userData.map((user, i) => {
+        // Generate HTML for each user in the menu
+        const html = `<li class="user_list p-3 flex rounded-lg m-2 transition-all duration-100" data-index='${i}' name='${user.login}'>
+            &nbsp;
+            <img
+            src="img/github-user.png"
+            alt="${user.login} png"
+            class="h-10 my-auto rounded-full" />
+
+            <h5 class="leading-10 transition duration-300 ">
+            &nbsp;${user.login}&nbsp;</h5>
+        </li>`;
+
+        menu.insertAdjacentHTML('beforeend', html);
+
+        // Load user avatar asynchronously
+        loadAvatar(user.avatar_url, menu.lastChild.querySelector('img'));
+
+        return menu.lastChild;
+    });
+};
+
+// Function to display search suggestions
+const displaySuggestions = async function (inputText) {
+    try {
+        userData = await fetch(`https://api.github.com/search/users?q=${inputText}&per_page=5&page=1`)
+            .then((res) => {
+                if (!res.ok) throw new Error('Data is not fetched');
+                return res.json();
+            })
+            .then((data) => data.items)
+            .catch((err) => {});
+
+        // Render the suggestion menu
+        renderMenu();
+    } catch (error) {
+        // Handle errors
+    }
+};
+
+/******************* Event Listeners ******************************/
+// Event listener to close the menu when clicking outside of it
+
+body.addEventListener('click', (e) => {
+    if (e.target == overlay) {
+        menu.style.display = 'none';
+        hideCard();
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'Escape') hideCard();
+});
+
+// Event listener to highlight the user on mouseover in the menu
+menu.addEventListener('mouseover', (e) => {
+    const ele = e.target.closest('.user_list');
+
+    if (ele) {
+        index = ele && e.target.closest('.user_list').dataset.index;
+        highlight(index);
+    }
+});
+
+menu.addEventListener('click', renderCard);
+
+// Event listener to handle keyboard interactions in the input field
+input.addEventListener('keydown', (event) => {
+    const key = event.key;
+    if (key == 'ArrowDown' || key == 'ArrowUp') {
+        // Handle arrow key navigation
+        if (key == 'ArrowDown') index++;
+        else {
+            if (index <= 0) index = userData.length - 1;
+            else index--;
+        }
+
+        index = index && index % userData.length;
+
+        // Highlight the selected user
+        highlight(index);
+    } else if (key == 'Enter') {
+        // Handle Enter key press
+        if (index == -1) index = 0;
+        if (userData.length) {
+            renderCard();
+        }
+    } else {
+        // Handle other key presses for search suggestions
+
+        index = -1;
+        clearTimeout(intervalId);
+        if (input.value.length > 1) {
+            intervalId = setTimeout(function () {
+                input.value.length && displaySuggestions(input.value);
+            }, 200);
+        } else deleteMenu();
+    }
+});
+
+// Event listener to display the menu on input click
+input.addEventListener('click', () => {
+    menu.style.display = '';
+    unselect();
+});
+
+userUsername.addEventListener('mouseover', (e) => {
+    linkArrow.classList.add('custom');
+});
+
+userUsername.addEventListener('mouseout', () => {
+    linkArrow.classList.remove('custom');
+});
+
+/**************** After Page loads. **********/
+loadBodyBackground();
